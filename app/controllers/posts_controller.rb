888f7ea0a88caafe5674dfc4610599f14549bf64
MarcAmato <marc.amato@hotmail.com>
2019-07-14 16:31:23 +0800

    adding bootstrap too views/application.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
 	# Create a method
 	def index
-
+		@posts = Post.all
 	end 
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	# This will take care of our new form
+	def new
+
+	end
+
+	def create
+		# render plain: params[:post].inspect
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+	end
+
+	private def post_params
+		# Only able to take in a title and a body
+		params.require(:post).permit(:title, :body)
+	end
 end
