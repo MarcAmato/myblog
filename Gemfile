2f2f4854ad29f99d1c0daf6e216bec84302ece7c
MarcAmato <marc.amato@hotmail.com>
2019-07-15 07:24:19 +0800

    navbar added

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Added 15 July 2019
+gem 'popper_js', '~> 1.14.5'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	# Look at the PostsController and then the index method
-	root 'posts#index'
+	root 'posts#index', as: 'home'
 
 	# Example of a regular route
-	get 'about' => 'pages#about'
+	get 'about' => 'pages#about', as: 'about'
 
 	# Maps HTTP verbs too controller actions
 	resources :posts
